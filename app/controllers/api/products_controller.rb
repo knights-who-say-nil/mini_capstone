d1f8926e3395c26bb99e8d670e1d6e67fe4ec69c
JackMarx <seriousfools@gmail.com>
2019-08-29 12:04:29 -0500

    one product route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,14 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jb'
   end
+
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jb'
+  end
+
+  def second_product
+    @product = Product.second
+    render 'second_product.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_products' => 'products#all_products'
+    get '/first_product' => 'products#first_product'
+    get '/second_product' => 'products#second_product'
   end
 end
