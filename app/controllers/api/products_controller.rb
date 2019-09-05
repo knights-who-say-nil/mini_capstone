8d4286c293b342ba79e2b4040a0b570357edbade
JackMarx <seriousfools@gmail.com>
2019-09-05 12:20:47 -0500

    happy/sad path update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -31,8 +31,11 @@ class Api::ProductsController < ApplicationController
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
 
-    @product.save
-    render 'show.json.jb'
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
